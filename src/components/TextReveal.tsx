5b402ae4166a55b41833a5624d06680041908e03
Artyom <artyomantonenko@gmail.com>
2024-09-02 23:38:47 +0200

    fixed deployment issues

--- src/components/TextReveal.tsx
+++
@@ -1,9 +0,0 @@
-import TextReveal from "@/components/magicui/text-reveal";
-
-export async function TextRevealDemo() {
-  return (
-    <div className="z-10 flex min-h-[16rem] items-center justify-center rounded-lg border bg-black">
-      <TextReveal text="Hi, I&apos;m Artyom. As a Full Stack Web Developer and UX/UI Designer based in Germany, I specialize in creating seamless digital experiences using Next.js. I&apos;m passionate about bringing innovative web applications to life, focusing on user-centric design. I&apos;ve worked on various projects and startups, excelling both independently and in teams. Whether working on a startup or a larger project, I am committed to crafting solutions that resonate with users." />
-    </div>
-  );
-}
